461f1c219eba104f8bbdc094e2868016728dd31e
Roman Pavliuk <sckalper@gmail.com>
2018-02-01 10:07:43 +0200

    #Skullper - added sample

--- app/src/main/java/ad/simple/MainActivity.java
+++ app/src/main/java/ad/simple/MainActivity.java
@@ -3,11 +3,15 @@ package ad.simple;
 import android.app.Activity;
 import android.os.Bundle;
 
+import ad.simple.library.BannerView;
+
 public class MainActivity extends Activity{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		BannerView bannerView = BannerView.newInstance("https://stackoverflow.com/questions/18097748/how-to-get-row-count-in-sqlite-using-android");
+		bannerView.show(getFragmentManager(), "BANNER");
 	}
 }
